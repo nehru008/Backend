77b0d9588205fbb46918bf527f2061d94d17a941
Nehru Sharma <nehruupsc@gmail.com>
2026-06-09 07:45:14 +0530

    WATCH HISTORY ADDED

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -4,6 +4,7 @@ import ApiResponse from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId) => {
     try {
@@ -592,6 +593,61 @@ const getUserCurrentProfile = asyncHandler(async (req, res) => {
     );
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        username: 1,
+                                        avatar: 1,
+                                        fullName: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ]);
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully"
+        )
+    )
+});
+
 // Request
 //    |
 //    v
@@ -624,5 +680,13 @@ export {
     LoginUser,
     LogOutUser,
     RefreshAccessToken,
-    ChangeCurrentPassword
+    ChangeCurrentPassword,
+    getCurrentUser,
+    getUserCurrentProfile,
+    getWatchHistory,
+    UpdateUserAvatar,
+    UpdateUserCoverImage,
+    updatedAccountDetails,
+    generateAccessAndRefreshTokens,
+
 };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,17 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js"; // import multer middleware
-import { LoginUser, LogOutUser, RegisterUser , RefreshAccessToken } from "../controllers/users.controllers.js";
+import { LoginUser, 
+        LogOutUser, 
+        RegisterUser , 
+        RefreshAccessToken ,
+        ChangeCurrentPassword, 
+        getCurrentUser, 
+        UpdateUserAvatar, 
+        UpdateUserCoverImage, 
+        getUserCurrentProfile, 
+        getWatchHistory, 
+        updatedAccountDetails
+} from "../controllers/users.controllers.js";
 import VerifyJWT from "../middlewares/auth.middleware.js";
 
 
@@ -26,5 +37,14 @@ router.route("/login").post(LoginUser)
 // secured routes
 router.route("/logout").post(VerifyJWT,LogOutUser)
 router.route("refresh-token").post(RefreshAccessToken)
+router.route("/change-password").post(VerifyJWT, ChangeCurrentPassword)
+router.route("/current-user").get(VerifyJWT, getCurrentUser)
+router.route("/update-account").patch(VerifyJWT, updatedAccountDetails)
+
+router.route("/avatar").patch(VerifyJWT, upload.single("avatar"), UpdateUserAvatar)
+router.route("/cover-image").patch(VerifyJWT, upload.single("coverImage"), UpdateUserCoverImage)
+
+router.route("/c/:username").get(VerifyJWT, getUserCurrentProfile)
+router.route("/history").get(VerifyJWT, getWatchHistory)
 
 export default router;
